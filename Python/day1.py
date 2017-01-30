85d93770f0b3cc258aa2695d720e5767424bfbb1
Bryan Dixon <javawolfpack@gmail.com>
2017-01-30 09:50:25 -0800

    Day 1 Python

---
+++ Python/day1.py
@@ -0,0 +1,25 @@
+print("Hello World")
+
+x = "hello"
+y = 3
+
+print(x+str(y))
+
+if True:
+    print("True")
+    print("Also True")
+else:
+    print("False")
+
+# if str(y)<x:
+#     print("Yes")
+
+a = [1,2,3]
+a += [4]
+# print(a)
+
+# for i in range(len(a)):
+#     print(a[i])
+
+for i in a:
+    print(i)
