aaaf75a728c98cc2ce8a9d4bd6b274d0a82c0705
Bryan Dixon <javawolfpack@gmail.com>
2017-02-08 13:53:10 -0800

    Added Static & Template examples

--- firstDjango/firstproject/firstapp/views.py
+++ firstDjango/firstproject/firstapp/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    context = {
+        'title':"Home",
+        'content': "Goodbye World"
+        }
+    return render(request,'home.html',context)
