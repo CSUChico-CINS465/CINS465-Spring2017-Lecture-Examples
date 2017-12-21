eab1cfc50898f87160afe6b37cc0ab6ce4ce0165
Bryan Dixon <javawolfpack@gmail.com>
2017-12-21 13:36:03 -0800

    remove login

--- login/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.Home),
-    url(r'^profile/$', views.update_profile),
-    url(r'^account/logout/$', views.Logout),
-]
